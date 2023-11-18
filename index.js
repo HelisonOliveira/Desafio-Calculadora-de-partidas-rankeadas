99ae269ea41e655e1e4c63b80140d536dbce2d0c
helisonoliveira <helisonoliveira@hotmail.com>
2023-11-18 19:23:44 -0300

    Criando o numero de derrotas

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 let numberOfVictories = Math.floor(Math.random() * 10)
-console.log(numberOfVictories);
+console.log("numeros de vitorias " + numberOfVictories);
 
-generateNumberOfDefeats()
+let numberOfDefeats = generateNumberOfDefeats(numberOfVictories)
+console.log("numeros de derrotas " + numberOfDefeats);
+
+generateNumberOfDefeats(numberOfVictories)
 function generateNumberOfDefeats(numberOfVictories) {
 
     if (numberOfVictories <= 10) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
-
         return numberOfDefeats
     }
 
 }
-
-console.log(numberOfDefeats);
