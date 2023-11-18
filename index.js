03ec8cc30e10922d8820fe068fbbce0e68a8f803
helisonoliveira <helisonoliveira@hotmail.com>
2023-11-18 19:44:20 -0300

    Criando as condicionais para os numeros de derrota

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 
-let numberOfVictories = Math.floor(Math.random() * 10)
+let numberOfVictories = Math.floor(Math.random() * 150)
 console.log("numeros de vitorias " + numberOfVictories);
 
 let numberOfDefeats = generateNumberOfDefeats(numberOfVictories)
 console.log("numeros de derrotas " + numberOfDefeats);
 
-generateNumberOfDefeats(numberOfVictories)
-function generateNumberOfDefeats(numberOfVictories) {
 
+function generateNumberOfDefeats(numberOfVictories) {
+    let numberOfDefeats = 0
     if (numberOfVictories <= 10) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
+    }else if (numberOfVictories <= 20) {
+        let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
+        return numberOfDefeats
+    }else if (numberOfVictories <= 50) {
+        let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
+        return numberOfDefeats
+    }else if (numberOfVictories <= 80) {
+        let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
+        return numberOfDefeats
+    }else if (numberOfVictories <= 90) {
+        let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
+        return numberOfDefeats
+    }else if (numberOfVictories <= 100) {
+        let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
+        return numberOfDefeats
+    } else if (numberOfVictories >= 101) {
+        let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
+        return numberOfDefeats
     }
-
 }
