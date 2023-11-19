fca1e33980d795e64ccaacae191efb2d5c9ec982
helisonoliveira <helisonoliveira@hotmail.com>
2023-11-18 22:51:37 -0300

    criando o gerador de nivel

--- index.js
+++ index.js
@@ -5,29 +5,57 @@ console.log("numeros de vitorias " + numberOfVictories);
 let numberOfDefeats = generateNumberOfDefeats(numberOfVictories)
 console.log("numeros de derrotas " + numberOfDefeats);
 
+let level = generateLevel(numberOfVictories)
+console.log(level);
 
 function generateNumberOfDefeats(numberOfVictories) {
     let numberOfDefeats = 0
     if (numberOfVictories <= 10) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
-    }else if (numberOfVictories <= 20) {
+    } else if (numberOfVictories <= 20) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
-    }else if (numberOfVictories <= 50) {
+    } else if (numberOfVictories <= 50) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
-    }else if (numberOfVictories <= 80) {
+    } else if (numberOfVictories <= 80) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
-    }else if (numberOfVictories <= 90) {
+    } else if (numberOfVictories <= 90) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
-    }else if (numberOfVictories <= 100) {
+    } else if (numberOfVictories <= 100) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
     } else if (numberOfVictories >= 101) {
         let numberOfDefeats = Math.floor(Math.random() * numberOfVictories)
         return numberOfDefeats
+    }
+}
+
+function generateLevel(numberOfVictories) {
+    let level = 0
+    if (numberOfVictories <= 10) {
+        let level = 10
+        return level
+    } else if (numberOfVictories <= 20) {
+        let level = 20
+        return level
+    } else if (numberOfVictories <= 50) {
+        let level = 50
+        return level
+    } else if (numberOfVictories <= 80) {
+        let level = 80
+        return level
+    } else if (numberOfVictories <= 90) {
+        let level = 90
+        return level
+    } else if (numberOfVictories <= 100) {
+        let level = 100
+        return level
+    } else if (numberOfVictories >= 101) {
+        let level = 150
+        return level
     }
 }
